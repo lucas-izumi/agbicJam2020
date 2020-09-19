b613f0ccac119ee9a5c940ee841e4ae139ea0d48
Lucas Izumi <lucasizumi@gmail.com>
2020-09-18 23:34:40 -0300

    Chain block match now working

--- Assets/Scripts/SolveBoard.cs
+++ Assets/Scripts/SolveBoard.cs
@@ -6,7 +6,17 @@ public class SolveBoard : MonoBehaviour
 {
     public string buttontag;
     public GameConfig gameConfig;
-    
+    private bool startMatching;
+    private int blockCount;
+    private Delay delay;
+
+    private void Start()
+    {
+        blockCount = GameObject.FindGameObjectsWithTag("gameblock").Length;
+        startMatching = false;
+        delay = new Delay(1.0f);
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -15,7 +25,17 @@ public class SolveBoard : MonoBehaviour
             Collider2D hitCollider = Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(pos));
             if (hitCollider != null && hitCollider.CompareTag(buttontag))
             {
+                startMatching = true;
+                delay.Reset();
+            }
+        }
+
+        if (startMatching)
+        {
+            if (delay.IsReady)
+            {
                 MatchAllBlocks();
+                delay.Reset();
             }
         }
     }
@@ -32,4 +52,18 @@ public class SolveBoard : MonoBehaviour
             }
         }
     }
+
+    /*private float CheckStillBlocks()
+    {
+        float stillBlocks = 0F;
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("gameblock"))
+        {
+            if (fooObj.name == "Block(Clone)")
+            {
+                stillBlocks += fooObj.GetComponent<Rigidbody2D>().velocity.magnitude;
+            }
+            
+        }
+        return stillBlocks;
+    }*/
 }
